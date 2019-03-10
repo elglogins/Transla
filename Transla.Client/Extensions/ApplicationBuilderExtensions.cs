45cc51e616ccb8bc2d49a8bdfc82a6da9939f399
elo <elg.logins@gmail.com>
2019-03-10 18:04:32 +0100

    Load dictionaries on client application startup

---
+++ Transla.Client/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Transla.Client.Services;
+
+namespace Transla.Client.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseTransla(this IApplicationBuilder app)
+        {
+            var dictionaryService = (IDictionaryService)app.ApplicationServices.GetService(typeof(IDictionaryService));
+            // ensure that latest application dictionaries are loaded
+            dictionaryService.Get("Hello","World");
+        }
+    }
+}
